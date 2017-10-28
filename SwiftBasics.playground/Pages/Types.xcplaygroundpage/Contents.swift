2d25ddbd14b496f09332a5b17678a3aafbc3d254
pjo901018 <pjo901018@korea.ac.kr>
2017-10-28 14:36:31 +0900

    Ch2-1 String interpolation

--- SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -1,8 +1,15 @@
-//: [Previous](@previous)
 // Chapter 2
 
-import Foundation
+let country  = "United States of America"
+let state = "North Carolina"
+let city = "Charlotte"
+let street = "West Street"
+let buildingNumber = 222
 
-var str = "Hello, playground"
+// Concatenation "A" + " B"
+let address = country + ", " + state + ", " + city
+//let streetAdress = buildingNumber + street <- error
 
-//: [Next](@next)
+// Interpolation
+let interpolatedAddress = "\(country), \(state), \(city)"
+let interpolatedStreetAdress = "\(buildingNumber) \(street)"
