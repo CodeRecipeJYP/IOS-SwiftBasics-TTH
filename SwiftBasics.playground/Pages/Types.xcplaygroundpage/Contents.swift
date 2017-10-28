aade4432753c8682392b482ca0c8f1e97d84f79f
pjo901018 <pjo901018@korea.ac.kr>
2017-10-28 14:42:04 +0900

    Ch2-3 types

--- SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -1,4 +1,7 @@
 // Chapter 2
+/*
+ Strings
+ */
 
 let country  = "United States of America"
 let state = "North Carolina"
@@ -13,3 +16,24 @@ let address = country + ", " + state + ", " + city
 // Interpolation
 let interpolatedAddress = "\(country), \(state), \(city)"
 let interpolatedStreetAdress = "\(buildingNumber) \(street)"
+
+
+/*
+ Integers
+ */
+
+let favoriteProgrammingLanguage = "Swift"
+let year = 2014 // Int
+
+/*
+ Floating Point Numbers
+ */
+
+// Double
+var version = 3.1
+
+/*
+ Boolean
+ */
+let isAwesome = true
+
